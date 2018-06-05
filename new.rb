d4767e4c8695f3abfcb13d9f9c20c778e5752b15
randihi <randihigashi@gmail.com>
2018-06-05 17:44:29 +0000

    automatically backed up by learn

---
+++ new.rb
@@ -0,0 +1,11 @@
+def say_hello
+  nam1 = "Charlie"
+  nam2 = "Randi"
+  nam3 = "Gabriella"
+  puts "Hello " + nam1 + "!"
+  puts "Hello " + nam2 + "!"
+  puts "Hello " + nam3 + "!"
+end
+
+say_hello
+
